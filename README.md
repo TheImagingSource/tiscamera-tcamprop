501192c9fce28e9953dd907fb6f0fe8c8105c0fb
Edgar Thier <edgar.thier@theimagingsource.com>
2020-08-04 12:26:09 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # tcamprop
 
 Linux introspection interface
+
+## Build
+
+### Dependencies
+
+Dependency versions are based on reference system
+
+libglib2.0-0 (>= 2.48.2)
+libgirepository-1.0-1 (>= 1.46.0)
+
+### Compile
+
+```
+    mkdir build
+    cd build
+    cmake ..
+    make
+    make package
+```
+
+## License
+
+All files in this project have to be considered open source under the Apache 2.0 license.
+
+New files have to contain the apache 2.0 license header
+
+## Missing
+
+Uinstall
---
+++ cmake/package-name.cmake
@@ -0,0 +1,109 @@
+# Copyright 2018 The Imaging Source Europe GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include(git-helper)
+include(CPackComponent)
+
+# The package name is created according to the following rules:
+#
+# If is is an official release on the master branch:
+#     tiscamera_MAJ.MIN.PAT_arch.deb
+#
+# If it is a preview release:
+#     tiscamera_MAJ.MIN.PAT.COMMIT_COUNT~branch_COMMIT_HASH_arch.deb
+#
+# If a git tag is present it will replace branch and commit hash
+#     tiscamera_MAJ.MIN.PAT.COMMIT_COUNT_tag_arch.deb
+#
+# If the built version is NOT a Release build,
+# the build type will be additionally appended.
+# this will always be lower case
+#     tiscamera_MAJ.MIN.PAT_arch_buildtype.deb
+#
+
+function (create_package_name return_value name version)
+
+  find_program(DPKG_PROGRAM dpkg DOC "dpkg program of Debian-based systems")
+  if(DPKG_PROGRAM)
+    execute_process(
+      COMMAND ${DPKG_PROGRAM} --print-architecture
+      OUTPUT_VARIABLE CPACK_DEBIAN_PACKAGE_ARCHITECTURE
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+
+    git_commit_tag(GIT_TAG)
+
+    set(build_type "")
+
+    if ("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
+
+      # empty since 'Release' is considered the default
+      set(build_type "")
+
+    else ()
+
+      # prepend a '_' this makes the rest of the name generation easier
+      # build_type will always be the last property in the string
+      # so no problems should be created by this
+      set(build_type "_${CMAKE_BUILD_TYPE}")
+
+    endif ("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
+
+    # we do not want '_Debug' as a string but '_debug'
+    string(TOLOWER "${build_type}" build_type)
+
+    if (GIT_TAG)
+
+
+      message("here?")
+      set(${return_value}
+        "${name}_${version}_${GIT_TAG}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${build_type}"
+        PARENT_SCOPE)
+
+    else ()
+
+      git_commit_hash(GIT_COMMIT_HASH)
+
+      if (GIT_COMMIT_HASH)
+
+        git_commit_count(GIT_COMMIT_COUNT)
+        git_branch(GIT_BRANCH)
+
+        if ("${GIT_BRANCH}" STREQUAL "master")
+          set(${return_value}
+            "${name}_${version}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${build_type}" PARENT_SCOPE)
+        else ()
+          set(${return_value}
+            "${name}_${version}.${GIT_COMMIT_COUNT}~${GIT_BRANCH}_${GIT_COMMIT_HASH}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${build_type}"
+            PARENT_SCOPE)
+
+        endif ("${GIT_BRANCH}" STREQUAL "master")
+
+      else () # this will be the case when users download the github zip and compiles on deb systems
+
+        set(${return_value}
+          "${name}_${version}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${build_type}" PARENT_SCOPE)
+
+      endif(GIT_COMMIT_HASH)
+
+    endif(GIT_TAG)
+
+  else(DPKG_PROGRAM)
+
+    set(${return_value}
+      "${name}_${version}_${CMAKE_SYSTEM_NAME}${build_type}" PARENT_SCOPE)
+
+  endif(DPKG_PROGRAM)
+
+endfunction ()
