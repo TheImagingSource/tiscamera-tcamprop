d607a8ea1b51c112f2b7b3d1ce0b83a60a81d7d7
Edgar Thier <edgar.thier@theimagingsource.com>
2021-03-01 08:26:11 +0100

    cmake: Update package name generation

--- cmake/package-name.cmake
+++ cmake/package-name.cmake
@@ -31,16 +31,57 @@ include(CPackComponent)
 # this will always be lower case
 #     tiscamera_MAJ.MIN.PAT_arch_buildtype.deb
 #
+# Manually setting CPACK_DEBIAN_PACKAGE_ARCHITECTURE will overwrite auto detection
+# CPACK_DEBIAN_PACKAGE_ARCHITECTURE is set to `dpkg --print-architecture`.
+# For Raspberry Pis the arch is appended with piX i.e. armhf_piX
+# where X is the pi version that is detected
+# To overwrite this set `PACKAGE_NAME_ONLY_ARCH` to ON
 
 function (create_package_name return_value name version)
 
-  find_program(DPKG_PROGRAM dpkg DOC "dpkg program of Debian-based systems")
-  if(DPKG_PROGRAM)
-    execute_process(
-      COMMAND ${DPKG_PROGRAM} --print-architecture
-      OUTPUT_VARIABLE CPACK_DEBIAN_PACKAGE_ARCHITECTURE
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
+  if(NOT CPACK_DEBIAN_PACKAGE_ARCHITECTURE)
+    find_program(EXECUTABLE_DPKG dpkg DOC "dpkg program of Debian-based systems")
+    if(EXECUTABLE_DPKG)
+      execute_process(
+        COMMAND ${EXECUTABLE_DPKG} --print-architecture
+        OUTPUT_VARIABLE CPACK_DEBIAN_PACKAGE_ARCHITECTURE
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+    endif(EXECUTABLE_DPKG)
+
+    string(COMPARE EQUAL "${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}" "armhf" IS_ARMHF)
+
+    # this check is to identify raspberry pi
+    # these systems receive special optimizations that are not always
+    # compatible to other armhf systems, thus needs special identification
+    if(IS_ARMHF AND NOT PACKAGE_NAME_ONLY_ARCH)
+
+      file(READ "/proc/cpuinfo" CPUINFO)
+
+      # cpuinfo should contain a model description
+      # if the OS does not offer that line use
+      # Hardware	: BCM2835 for PI4
+      # Hardware    : BCM2708 for PI1
+      # Hardware    : BCM2709 for PI2 and PI3
+      string(FIND "${CPUINFO}" "Raspberry Pi 3" IS_PI3)
+
+      # find returns -1 if not found
+      # -1 is not considered false
+      if(IS_PI3 GREATER 0)
+        string(APPEND CPACK_DEBIAN_PACKAGE_ARCHITECTURE "_pi3")
+      endif(IS_PI3 GREATER 0)
+
+      string(FIND "${CPUINFO}" "Raspberry Pi 4" IS_PI4)
+
+      if(IS_PI4 GREATER 0)
+        string(APPEND CPACK_DEBIAN_PACKAGE_ARCHITECTURE "_pi4")
+      endif(IS_PI4 GREATER 0)
+
+    endif(IS_ARMHF AND NOT PACKAGE_NAME_ONLY_ARCH)
+
+  endif(NOT CPACK_DEBIAN_PACKAGE_ARCHITECTURE)
+
+  if(CPACK_DEBIAN_PACKAGE_ARCHITECTURE)
 
     git_commit_tag(GIT_TAG)
 
@@ -63,10 +104,16 @@ function (create_package_name return_value name version)
     # we do not want '_Debug' as a string but '_debug'
     string(TOLOWER "${build_type}" build_type)
 
+    set(distribution "")
+    if (TCAM_DISTRIBUTION_DESCRIPTION)
+
+      set(distribution "_${TCAM_DISTRIBUTION_DESCRIPTION}")
+
+
+    endif (TCAM_DISTRIBUTION_DESCRIPTION)
+
     if (GIT_TAG)
 
-
-      message("here?")
       set(${return_value}
         "${name}_${version}_${GIT_TAG}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${build_type}"
         PARENT_SCOPE)
@@ -82,10 +129,10 @@ function (create_package_name return_value name version)
 
         if ("${GIT_BRANCH}" STREQUAL "master")
           set(${return_value}
-            "${name}_${version}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${build_type}" PARENT_SCOPE)
+            "${name}_${version}.${GIT_COMMIT_COUNT}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${distribution}${build_type}" PARENT_SCOPE)
         else ()
           set(${return_value}
-            "${name}_${version}.${GIT_COMMIT_COUNT}~${GIT_BRANCH}_${GIT_COMMIT_HASH}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${build_type}"
+            "${name}_${version}.${GIT_COMMIT_COUNT}~${GIT_BRANCH}_${GIT_COMMIT_HASH}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${distribution}${build_type}"
             PARENT_SCOPE)
 
         endif ("${GIT_BRANCH}" STREQUAL "master")
@@ -93,17 +140,17 @@ function (create_package_name return_value name version)
       else () # this will be the case when users download the github zip and compiles on deb systems
 
         set(${return_value}
-          "${name}_${version}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${build_type}" PARENT_SCOPE)
+          "${name}_${version}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}${distribution}${build_type}" PARENT_SCOPE)
 
       endif(GIT_COMMIT_HASH)
 
     endif(GIT_TAG)
 
-  else(DPKG_PROGRAM)
+  else(CPACK_DEBIAN_PACKAGE_ARCHITECTURE)
 
     set(${return_value}
       "${name}_${version}_${CMAKE_SYSTEM_NAME}${build_type}" PARENT_SCOPE)
 
-  endif(DPKG_PROGRAM)
+  endif(CPACK_DEBIAN_PACKAGE_ARCHITECTURE)
 
 endfunction ()
